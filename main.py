6d1e99335986395d2269723dcafbcd22c0602284
asif-kamal <asif.kamal1489@gmail.com>
2025-11-12 19:25:16 -0600

    Quiz project - created QuizBrain class

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-class Question:
-    def __int__(self, text, answer):
-        self.text = text
-        self.answer = answer
+import data
+import question_model
+from quiz_brain import QuizBrain
+
+question_bank = []
+for num in range(0, len(data.question_data)):
+    new_q = question_model.Question(text=data.question_data[num]["text"],
+                                    answer=data.question_data[num]["answer"])
+    question_bank.append(new_q)
+
+quiz = QuizBrain(question_bank)
+quiz.next_question()
