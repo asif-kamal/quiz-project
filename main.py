8a6178fe833c6f3625a5234b858b9baf202a3eb8
asif-kamal <asif.kamal1489@gmail.com>
2025-11-13 19:18:26 -0600

    Quiz project - data manipulation via API

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-import data
+# import data
 import question_model
 from quiz_brain import QuizBrain
+import requests
 
+api_url = "https://opentdb.com/api.php?amount=50&difficulty=medium&type=boolean"
 question_bank = []
-for num in range(0, len(data.question_data)):
-    new_q = question_model.Question(text=data.question_data[num]["text"],
-                                    answer=data.question_data[num]["answer"])
-    question_bank.append(new_q)
+
+try:
+    response = requests.get(api_url)
+    response.raise_for_status()
+
+    data = response.json()
+
+    for num in range(0, len(data['results'])):
+        new_q = question_model.Question(text=data['results'][num]["question"],
+                                        answer=data['results'][num]["correct_answer"])
+        question_bank.append(new_q)
+
+
+except requests.exceptions.RequestException as e:
+    print(f"An error occurred while fetching data from the API: {e}")
 
 quiz = QuizBrain(question_bank)
 
